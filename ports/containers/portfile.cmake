62c3a38c12aa8c3970151d3c3d71ca7943d8a1b8
Matthew Love <matthewlove94@gmail.com>
2025-06-12 17:40:39 +0100

    containers port to 0.2.1

--- ports/containers/portfile.cmake
+++ ports/containers/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nukethebees/containers
-    REF v0.2.0
-    SHA512 69dea20fa738371a1f32b80e661ed966ba8fc8547ceca6aee4fb4e9a97d196dda85e6cfc3cfce2aaab7017aa0a60c4632496d3ca310fa8c7156bd155d5f82be5
+    REF v0.2.1
+    SHA512 a4e488d8d4cd0e8ce12cd7d81b0125e868f28475f23b90fb59630f55a28ac6e6ea39739161cdb5b77e925837be44d59369c3c3492aea298d0d80b4f033b29da0
     HEAD_REF master
 )
 
